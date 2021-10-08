1dd9c366b885725a3081726b05732a2b81a8e6c7
Aleksa Vučković <aleksav013@gmail.com>
2021-10-09 01:22:30 +0200

    Small changes before adding gdt

--- Makefile
+++ Makefile
@@ -1,37 +1,34 @@
-CC = i686-elf-gcc
-AS = i686-elf-as
-CFLAGS = 
+CC=i686-elf-gcc
+AS=i686-elf-as
+CFLAGS=-ffreestanding -O2 -Wall -Wextra
 
-MKDIR = mkdir -p
-RM = rm -rf
-CP = cp
+MKDIR=mkdir -p
+RM=rm -rf
+CP=cp
 
-SOURCE_DIR = src
-BUILD_DIR = build
-ISO_DIR = isodir
+SOURCE_DIR=src
+BUILD_DIR=build
+ISO_DIR=isodir
 
-AS_SOURCE = boot.s
-C_SOURCE = kernel.c
+TARGET=myos
 
-OBJ_FILES = boot.o kernel.o
-CRTBEGIN_OBJ:=$(shell $(CC) -print-file-name=crtbegin.o)
-CRTEND_OBJ:=$(shell $(CC) -print-file-name=crtend.o)
-
-OBJ = $(BUILD_DIR)/crti.o $(CRTBEGIN_OBJ) $(patsubst %,$(BUILD_DIR)/%,$(OBJ_FILES)) $(CRTEND_OBJ) $(BUILD_DIR)/crtn.o
+OBJ_FILES=boot.o kernel.o
+CRTBEGIN_OBJ=$(shell $(CC) -print-file-name=crtbegin.o)
+CRTEND_OBJ=$(shell $(CC) -print-file-name=crtend.o)
+OBJ=$(BUILD_DIR)/crti.o $(CRTBEGIN_OBJ) $(patsubst %,$(BUILD_DIR)/%,$(OBJ_FILES)) $(CRTEND_OBJ) $(BUILD_DIR)/crtn.o
 
 # Creating iso file
-.PHONY: all
-all: $(BUILD_DIR)/myos.bin
+$(TARGET).iso: $(BUILD_DIR)/$(TARGET).bin
 	grub-file --is-x86-multiboot $(BUILD_DIR)/myos.bin
 	mkdir -p $(ISO_DIR)/boot/grub
 	$(CP) $(BUILD_DIR)/myos.bin $(ISO_DIR)/boot/myos.bin
 	$(CP) $(SOURCE_DIR)/grub.cfg $(ISO_DIR)/boot/grub/grub.cfg
-	grub-mkrescue -o myos.iso $(ISO_DIR)
+	grub-mkrescue -o $(TARGET).iso $(ISO_DIR)
 
 # Linking object files
-$(BUILD_DIR)/myos.bin: $(OBJ)
+$(BUILD_DIR)/$(TARGET).bin: $(OBJ)
 	$(MKDIR) $(BUILD_DIR)
-	$(CC) -T $(SOURCE_DIR)/linker.ld -o $(BUILD_DIR)/myos.bin -ffreestanding -O2 -nostdlib $(OBJ) -lgcc
+	$(CC) -T $(SOURCE_DIR)/linker.ld -o $@ $(CFLAGS) -nostdlib $^ -lgcc
 
 # Compiling as sources
 $(BUILD_DIR)/%.o: $(SOURCE_DIR)/%.s
@@ -41,9 +38,14 @@ $(BUILD_DIR)/%.o: $(SOURCE_DIR)/%.s
 # Compiling C sources
 $(BUILD_DIR)/%.o: $(SOURCE_DIR)/%.c
 	$(MKDIR) $(BUILD_DIR)
-	$(CC) -c $< -o $@ -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+	$(CC) -c $< -o $@ -std=gnu99 $(CFLAGS)
 
-# Cleaning
+# Boot kernel in qemu
+.PHONY: run
+run: $(TARGET).iso
+	qemu-system-x86_64 -cdrom $(TARGET).iso
+
+# Clean build files
 .PHONY: clean
 clean:
-	$(RM) $(BUILD_DIR) $(ISO_DIR) myos.iso
+	$(RM) $(BUILD_DIR) $(ISO_DIR) $(TARGET).iso
--- src/kernel.c
+++ src/kernel.c
@@ -1,37 +1,11 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
- 
-/* Check if the compiler thinks you are targeting the wrong operating system. */
-#if defined(__linux__)
-#error "You are not using a cross-compiler, you will most certainly run into trouble"
-#endif
- 
-/* This tutorial will only work for the 32-bit ix86 targets. */
-#if !defined(__i386__)
-#error "This tutorial needs to be compiled with a ix86-elf compiler"
-#endif
- 
-/* Hardware text mode color constants. */
-enum vga_color {
-    VGA_COLOR_BLACK = 0,
-    VGA_COLOR_BLUE = 1,
-    VGA_COLOR_GREEN = 2,
-    VGA_COLOR_CYAN = 3,
-    VGA_COLOR_RED = 4,
-    VGA_COLOR_MAGENTA = 5,
-    VGA_COLOR_BROWN = 6,
-    VGA_COLOR_LIGHT_GREY = 7,
-    VGA_COLOR_DARK_GREY = 8,
-    VGA_COLOR_LIGHT_BLUE = 9,
-    VGA_COLOR_LIGHT_GREEN = 10,
-    VGA_COLOR_LIGHT_CYAN = 11,
-    VGA_COLOR_LIGHT_RED = 12,
-    VGA_COLOR_LIGHT_MAGENTA = 13,
-    VGA_COLOR_LIGHT_BROWN = 14,
-    VGA_COLOR_WHITE = 15,
-};
- 
+#include"vga.h"
+
+static const size_t VGA_WIDTH = 80;
+static const size_t VGA_HEIGHT = 25;
+
 static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) 
 {
     return fg | bg << 4;
@@ -41,16 +15,6 @@ static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
 {
     return (uint16_t) uc | (uint16_t) color << 8;
 }
- 
-size_t strlen(const char* str) 
-{
-    size_t len = 0;
-    while (str[len]) len++;
-    return len;
-}
- 
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 25;
  
 size_t terminal_row;
 size_t terminal_column;
@@ -63,17 +27,14 @@ void terminal_initialize(void)
     terminal_column = 0;
     terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
     terminal_buffer = (uint16_t*) 0xB8000;
-    for (size_t y = 0; y < VGA_HEIGHT; y++) {
-	for (size_t x = 0; x < VGA_WIDTH; x++) {
+    for (size_t y = 0; y < VGA_HEIGHT; y++)
+    {
+	for (size_t x = 0; x < VGA_WIDTH; x++)
+	{
 	    const size_t index = y * VGA_WIDTH + x;
 	    terminal_buffer[index] = vga_entry(' ', terminal_color);
 	}
     }
-}
- 
-void terminal_setcoor(uint8_t color) 
-{
-    terminal_color = color;
 }
  
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) 
@@ -84,11 +45,8 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 
 void movescreen()
 {
-    if (terminal_row == VGA_HEIGHT)
-    {
-	terminal_row--;
-	for(size_t i=0;i<VGA_HEIGHT;i++) for(size_t j=0;j<VGA_WIDTH;j++) terminal_buffer[i*VGA_WIDTH+j]=terminal_buffer[(i+1)*VGA_WIDTH+j];
-    }
+    terminal_row--;
+    for(size_t i=0;i<VGA_HEIGHT;i++) for(size_t j=0;j<VGA_WIDTH;j++) terminal_buffer[i*VGA_WIDTH+j]=terminal_buffer[(i+1)*VGA_WIDTH+j];
 }
  
 void terminal_putchar(char c) 
@@ -102,30 +60,18 @@ void terminal_putchar(char c)
     if (terminal_row == VGA_HEIGHT) movescreen();
 }
  
-void terminal_write(const char* data, size_t size) 
-{
-    for (size_t i = 0; i < size; i++) terminal_putchar(data[i]);
-}
- 
 void terminal_writestring(const char* data) 
 {
-    terminal_write(data, strlen(data));
+    for(int i=0;data[i]!='\0';i++) terminal_putchar(data[i]);
 }
 
-char *rec;
-__attribute__ ((constructor)) void foo(void)
-{
-    rec="aleksa";
-}
- 
 void kernel_main(void) 
 {
     terminal_initialize();
 
-    for(size_t i=0;i<50;i++)
-    {
-	for(size_t j=0;j<i;j++) terminal_writestring("#");
-	terminal_writestring("Hello, kernel World!\n");
-    }
-    terminal_writestring(rec);
+   for(size_t i=0;i<50;i++)
+   {
+       for(size_t j=0;j<i;j++) terminal_writestring("#");
+       terminal_writestring("Hello, kernel World!\n");
+   }
 }
